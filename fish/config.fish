738be67c816d05a7cd9ae6dfec7ad4538f4c5c1a
Matt Daue <mattdaue@gmail.com>
2014-11-16 15:53:22 -0500

    Add i3 and fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,11 @@ function j
     cd (command autojump $argv)
 end
 
+alias addgc='ssh-add ~/.ssh/git_carbyne'
+alias addh='ssh-add ~/.ssh/id_home'
+alias addc='ssh-add ~/.ssh/id_carbyne'
+alias vi='vim'
+
+set -x TERMINAL roxterm
+set -x EDITOR vim
+
