e73ba3b279b96424c26d480be1b214abf4345a3f
Matt Daue <mattdaue@gmail.com>
2014-11-26 11:08:15 -0500

    fix config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,10 @@
-function j
-    cd (command autojump $argv)
-end
-
+# Alias definitions
 alias addgc='ssh-add ~/.ssh/git_carbyne'
 alias addh='ssh-add ~/.ssh/id_home'
 alias addc='ssh-add ~/.ssh/id_carbyne'
 alias vi='vim'
 
+# Exports
 set -x TERMINAL roxterm
 set -x EDITOR vim
 
