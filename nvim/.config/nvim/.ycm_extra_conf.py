b3abb23dec0f8a7b0214d6309a27fc452c7c2779
Matt Daue <mattdaue@gmail.com>
2017-08-12 13:34:59 -0400

    Add ycm_extra_conf.py
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

---
+++ nvim/.config/nvim/.ycm_extra_conf.py
@@ -0,0 +1,26 @@
+import os
+import ycm_core
+
+flags = [
+  '-Wall',
+  '-Wextra',
+  '-Werror',
+  '-Wno-long-long',
+  '-Wno-variadic-macros',
+  '-fexceptions',
+  '-ferror-limit=10000',
+  '-DNDEBUG',
+  '-std=c99',
+  '-xc',
+  '-isystem/usr/include/',
+  '-i~/dev/arrow/Arrow/',
+  '-i~/dev/fusion/Fusion/'
+  ]
+
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', ]
+
+def FlagsForFile( filename, **kwargs ):
+  return {
+  'flags': flags,
+  'do_cache': True
+  }
